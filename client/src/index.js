514d74fefb8d050a8ad1773d2aed2494ee93bce6
KesaraAnanya <147529035+KesaraAnanya@users.noreply.github.com>
2025-04-26 21:57:39 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+if (window.location.pathname !== '/' && !window.location.hash) {
+  window.location.replace(window.location.origin + '/#/');
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 reportWebVitals();
